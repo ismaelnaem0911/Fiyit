0f18d12aad7f700295c2d1e611878608ae9c71b6
ismaelnaem0911 <ismaelnaem0911@gmail.com>
2026-06-11 07:46:11 +0300

    Delete js/books.js

--- js/books.js
+++
@@ -1,9 +0,0 @@
-// js/books.js - Keep navigation logic, just update the API call
-async function openUnit(unitName, isComingSoon) {
-    // ... [KEEP YOUR EXISTING NAVIGATION CODE HERE] ...
-    
-    const savedKey = localStorage.getItem('fiyit_gemini_key');
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${savedKey}`;
-
-    // ... [REST OF YOUR FETCH LOGIC] ...
-}
